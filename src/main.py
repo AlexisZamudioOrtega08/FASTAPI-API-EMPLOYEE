88e5a9d669a284b050cdcee8e28329c305dc08a8
Alexis Zamudio <alexis.zamudio.ortega@gmail.com>
2022-05-10 21:33:45 -0500

    Adding schemas for data validation

--- src/main.py
+++ src/main.py
@@ -2,17 +2,16 @@ from fastapi import FastAPI, HTTPException, Depends
 import models
 from database import SessionLocal, engine
 from sqlalchemy.orm import Session
+from schemas import Employee
 
 app = FastAPI()
-
-employees = []
 
 models.Base.metadata.create_all(bind=engine)
 
 
 def get_db() -> Session:
     """
-    Complete the full cycle of a session (open, close).
+    Complete the full cycle of a db session (open, close).
     """
     try:
         db = SessionLocal()
@@ -24,18 +23,18 @@ def get_db() -> Session:
 @app.get("/api/employees/", status_code=200)
 async def get_employees(db: Session = Depends(get_db)):
     """
-    Get all employees
+    Get all employees registered in the database.
     """
     return db.query(models.Employee).all()
 
 
 @app.post("/api/employee", status_code=201)
-async def add_employee(emp_id: int, emp_name: str, db: Session = Depends(get_db)):
+async def add_employee(employee: Employee, db: Session = Depends(get_db)):
     """
     Add a new employee to the database
     """
     try:
-        new_employee = models.Employee(id=emp_id, name=emp_name)
+        new_employee = models.Employee(id=employee.id, name=employee.name)
         db.add(new_employee)
         db.commit()
         return {"message": "Employee added"}
---
+++ src/schemas.py
@@ -0,0 +1,17 @@
+import string
+from pydantic import BaseModel, validator
+
+
+class Employee(BaseModel):
+    id: int
+    name: str
+
+    @validator("name")
+    def validate_name(cls, v):
+        if not v:
+            raise ValueError("Name is required")
+        if not all(c in string.ascii_letters for c in v):
+            raise ValueError("Name must be alphabetic")
+        if len(v) < 3:
+            raise ValueError("Name must be at least 3 characters long")
+        return v
