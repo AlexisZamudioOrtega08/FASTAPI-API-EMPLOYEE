bc572ed72beab5da31b6bdbba7987c0922fd4edf
Alexis Zamudio <alexis.zamudio.ortega@gmail.com>
2022-05-10 20:56:19 -0500

    Adding modification to create a sqlite db file and store employees
    database.py and models.py are being added to be able to handle db (create table, connection).

--- src/main.py
+++ src/main.py
@@ -1,30 +1,48 @@
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
+import models
+from database import SessionLocal, engine
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-employees = [
-    {"id": 1, "name": "John"},
-    {"id": 2, "name": "Doe"},
-    {"id": 3, "name": "Smith"},
-]
+employees = []
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db() -> Session:
+    """
+    Complete the full cycle of a session (open, close).
+    """
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/api/employees/", status_code=200)
-async def get_employees() -> Response:
+async def get_employees(db: Session = Depends(get_db)):
     """
     Get all employees
     """
-    return [employee for employee in employees]
+    return db.query(models.Employee).all()
 
 
 @app.post("/api/employee", status_code=201)
-async def add_employee(emp_id: int, emp_name: str) -> Response:
+async def add_employee(emp_id: int, emp_name: str, db: Session = Depends(get_db)):
     """
-    Add an employee to the list
+    Add a new employee to the database
     """
-    employee = {"id": emp_id, "name": emp_name}
-    if id not in [employee["id"] for employee in employees]:
-        employees.append(employee)
-        return employee
-    else:
-        raise HTTPException(status_code=400, detail="Employee already exists")
+    try:
+        new_employee = models.Employee(id=emp_id, name=emp_name)
+        db.add(new_employee)
+        db.commit()
+        return {"message": "Employee added"}
+    except Exception as e:
+        if "UNIQUE constraint failed" in str(e):
+            raise HTTPException(
+                status_code=400, detail="An employee with provided id already exists"
+            )
+        else:
+            raise HTTPException(status_code=500, detail="Internal server error")
